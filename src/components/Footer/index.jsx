fc2aea07f0c4439430c2591ffe1ca3ff732e5baf
svandu <svandu08@gmail.com>
2023-04-12 19:23:09 +0530

    footer add

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "./footer.css";
+
+
+function Footer() {
+  return (
+    <div className="footer-main-container">
+      <div className="contact-us-section">
+        <p className="contact-us">Contact Us</p>
+        <p className="pnone-number">+91 9573684125</p>
+        <p className="email-id">Email Id</p>
+        <p className="email">taxi@gmail.com</p>
+      </div>
+      <div className="footer-links">
+        <p className="our-links">Our Links</p>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Facility</a>
+          </li>
+        </ul>
+      </div>
+      <div className="social-media-links">
+        <ul>
+          <li>
+            <svg
+              width="46"
+              height="46"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              width="46"
+              height="46"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+              <path d="M17.5 6.5h.01"></path>
+            </svg>
+          </li>
+          <li>
+            <svg
+              width="46"
+              height="46"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83A7.72 7.72 0 0 0 23 3Z"></path>
+            </svg>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
